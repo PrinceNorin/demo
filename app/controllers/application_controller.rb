bde26c4aa9ce3a9e57a7ea936d2cce98cabb55c0
Hor Norin <norin.hor@gmail.com>
2015-02-05 20:52:14 +0700

    remove space

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
   
   include SessionsHelper
   
@@ -13,8 +15,6 @@ class ApplicationController < ActionController::Base
         redirect_to signin_path
       end
     end
-    
-
     
     def redirect_signed_in_user
       if user_signed_in?
